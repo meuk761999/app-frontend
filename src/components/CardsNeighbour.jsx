df0a1438528563fc3bb024668e0fd5d254bc2b1e
mukul <mukulawasthi761999@gmail.com>
2023-12-20 01:31:34 +0530

    resolving Error: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.  react/no-unescaped-entities

--- src/components/CardsNeighbour.jsx
+++ src/components/CardsNeighbour.jsx
@@ -6,14 +6,14 @@ const CardsNeighbour = () => {
   return (
     <div className='bg-gradient-to-tr from-purple-950 via-pink-900 to-violet-950
                           p-2 w-full h-full  group hover:scale-105 hover:rounded-3xl group duration-500 overflow-hidden'>
-            <div className='full; w-full h-full bg-black p-6 m-0  group-hover:rounded-2xl group groups duration-500 overflow-hidden'>
+            <div className='w-full h-full bg-black p-6 m-0  group-hover:rounded-2xl group groups duration-500 overflow-hidden'>
                 <p className=" overflow-hidden whitespace-nowrap pb-2
                               text-sm sm:text-1xl md:text-2xl  text-white font-bold select-none">
                      &lt; We Code For You /&gt;
                 </p>
                 <div className='w-full h-1 rounded bg-gradient-to-r from-purple-900 via-pink-900 to-black'></div>
                 <p className=" break-words text-sm sm:text-xl font-thin py-4 ">
-                <span className='font-bold text-violet-900 text-3xl'>Xenture</span> brings over 7 years of experience in the FinTech sector, a solid track record, and enduring integration background in enterprise-grade projects to our client's disposal
+                <span className='font-bold text-violet-900 text-3xl'>Xenture</span> brings over 7 years of experience in the FinTech sector, a solid track record, and enduring integration background in enterprise-grade projects to our clients disposal
                 </p>
                 <ClickButton/>
              </div>
